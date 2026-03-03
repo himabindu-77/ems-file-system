cfb03f671d03e6989fb91cee0504779451d26955
himabindu-77 <himabindu.a@tektalis.com>
2026-03-03 22:47:28 +0530

    refactored

--- myproject/src/com/ems/Main/Main.java
+++ myproject/src/com/ems/Main/Main.java
@@ -3,7 +3,6 @@ package com.ems.Main;
 public class Main {
 
 	public static void main(String[] args) {
-		//FileUtil.loadEmployees(EmsCrudOpsMap.empMap);
 		Menu menu = new Menu();
 		menu.start();
 	}
